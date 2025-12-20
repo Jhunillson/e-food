d8a910cbbb4158d1353885a653aa6e857f8e56fa
Junilson Kaputo <josejunilsonfernando@gmail.com>
2025-12-20 20:05:08 +0100

    Use DATABASE_URL for Railway and fix SSL issue

--- efood-backend/src/config/database.js
+++ efood-backend/src/config/database.js
@@ -1,23 +1,37 @@
 require('dotenv').config();
 
+const useSSL = !!process.env.DATABASE_URL;
+
 const baseConfig = {
   dialect: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
+  logging: false
 };
 
 module.exports = {
-  development: baseConfig,
-  production: baseConfig,
-  test: baseConfig
+  development: {
+    ...baseConfig,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  },
+
+  production: process.env.DATABASE_URL
+    ? {
+        ...baseConfig,
+        url: process.env.DATABASE_URL
+      }
+    : {
+        ...baseConfig,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+      },
+
+  test: {
+    ...baseConfig
+  }
 };
